ac8b3ce7f9a5172f0b1458124c6c28d09793a665
Marian Keller <marian@laphtec.com>
2022-11-29 06:56:17 +0100

    added Ajax

--- public/javascripts/newgame.js
+++ public/javascripts/newgame.js
@@ -1,7 +1,8 @@
-
 document.getElementById("add_name_button").addEventListener("click", addName);
 document.getElementById("name_textfield").addEventListener("change", addName);
+document.getElementById("start_btn").addEventListener("click", startGame)
 
+var playerNameList = [];
 
 function addName() {
     var nameField = document.getElementById("name_textfield")
@@ -9,6 +10,7 @@ function addName() {
     var li = document.createElement("li");
     var name = nameField.value;
     if (name != "") {
+        playerNameList.push(name);
         nameField.value = "";
 
         li.appendChild(document.createTextNode(name));
@@ -17,5 +19,34 @@ function addName() {
         setTimeout(function() {
             li.className = li.className + " show";
         }, 10);
+    }
+}
+
+function evaluate(input, cFunction) {
+  $.ajax({
+    url:"eval/"+input,
+    type:"POST",
+    contentType:"text/plain",
+    success: function() {cFunction();}
+  })
+}
+
+function startGame() {
+    if (playerNameList.length <= 0) {
+        alert("You seem to be quite alone, please add some players.");
+        return;
+    }
+    evaluate(playerNameList.length.toString(), startGame2);
+}
+
+function startGame2() {
+    evaluate("weiter", startGame3);
+}
+
+function startGame3() {
+    if (playerNameList.length > 0) {
+        evaluate(playerNameList.pop(), startGame3);
+    } else {
+        $(location).prop('href', '/gameScreen');
     }
 }
